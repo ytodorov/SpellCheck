9679a60da424a614df3c3841fff108e5f7abd399
ytodorov <ytodorov@ytodorov.com>
2016-03-06 13:43:26 +0200

    initial commit with files

---
+++ SpellCheckMeOnlineWeb/SpellCheckMeOnlineWeb/Controllers/HomeController.cs
@@ -0,0 +1,156 @@
+﻿using SpellCheckMeOnlineWeb.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpellCheckMeOnlineWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetHtml(string url)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string html = httpClient.GetStringAsync(url).Result;
+
+                //?
+                if (!url.EndsWith("/"))
+                {
+                    url += "/";
+                }
+                html = html.Replace("href=\"/", "href=\"" + url);
+                html = html.Replace("src=\"/", "src=\"" + url);
+
+                string urlBase = new Uri(url).OriginalString;
+
+                var indexOfHead = html.IndexOf("<head");
+                for (int i = indexOfHead; i < html.Length; i++)
+                {
+                    char currentChar = html[i];
+                    if (currentChar.Equals('>'))
+                    {
+                        indexOfHead = i;
+                        break;
+                    }
+                }
+
+                html = html.Insert(indexOfHead + 1, "<base href='" + urlBase + "'>");
+                return Json(html, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult SpellText(string htmlEncoded, string text, string langugage)
+        {
+            string html = Server.HtmlDecode(htmlEncoded);
+
+            var chars = text.ToCharArray();
+            List<string> emptyChars = new List<string>();
+            foreach (char c in chars)
+            {
+                if (string.IsNullOrWhiteSpace(c.ToString()))
+                {
+                    emptyChars.Add(c.ToString());
+                }
+            }
+
+            var words = text.Replace("\n", " ").Replace(",", " ").Replace(".", " ").Split(emptyChars.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> wrongWords = new List<string>();
+            words = words.Select(w => w.Trim()).Where(s => !string.IsNullOrWhiteSpace(s)).Where(w => ContainsLetters(w)).ToList();
+            foreach (string word in words)
+            {
+                var res = SpellEngineManager.SpellEngine[langugage].Spell(word);
+                if (!res)
+                {
+                    wrongWords.Add(word);
+                }
+            }
+            wrongWords = wrongWords.Distinct().ToList();
+
+            StringBuilder sbSummary = new StringBuilder();
+            string begin = @"  <table id='grid'>
+                <colgroup>
+                    <col />
+                    <col />                    
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th data-field='make'>Word</th>
+                        <th data-field='model'>Suggestion</th>                        
+                    </tr>
+                </thead>
+                <tbody>";
+            sbSummary.Append(begin);
+
+            foreach (string word in wrongWords)
+            {
+                var suggest = SpellEngineManager.SpellEngine[langugage].Suggest(word);
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in suggest)
+                {
+                    sb.Append(item + ", ");
+                   
+                }               
+                string suggestions = sb.ToString();
+                sbSummary.Append(CreateGridRow(word, suggestions));
+
+               
+
+                html = html.Replace(word, $"<span title='{suggestions}' style='color:blueviolet;background-color:yellow'>{word}</span>");
+            }
+
+            sbSummary.Append("</tbody></table>");
+
+            var returnObject = new { html = html + "<div></div>", summary = sbSummary.ToString() };
+            return Json(returnObject);
+        }
+
+        bool ContainsLetters(string wordToCheck)
+        {
+            var arr = wordToCheck.ToCharArray();
+            foreach (char c in arr)
+            {
+                if (char.IsLetter(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        string CreateGridRow(string word, string suggestion)
+        {
+            string row = $@"<tr>
+                        <td>{word}</td>
+                        <td>{suggestion}</td>
+                            </tr>";
+            return row;
+                        
+        }
+    }
+}
---
+++ SpellCheckMeOnlineWeb/SpellCheckMeOnlineWeb/Infrastructure/SpellEngineManager.cs
@@ -0,0 +1,48 @@
+﻿using NHunspell;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SpellCheckMeOnlineWeb.Infrastructure
+{
+    public static class SpellEngineManager
+    {
+        public static SpellEngine SpellEngine { get; set; }
+        static SpellEngineManager()
+        {
+            try
+            {
+                string dictionaryPath = Hunspell.NativeDllPath;
+
+                DirectoryInfo di = new DirectoryInfo(dictionaryPath);
+                FileInfo[] files = di.GetFiles("*", SearchOption.AllDirectories);
+
+                SpellEngine = new SpellEngine();
+                LanguageConfig enConfig = new LanguageConfig();
+                enConfig.LanguageCode = "en";
+                enConfig.HunspellAffFile = files.FirstOrDefault(f => f.FullName.EndsWith("en_us.aff", StringComparison.InvariantCultureIgnoreCase)).FullName; //Path.Combine(dictionaryPath, "en_us.aff");
+                enConfig.HunspellDictFile = files.FirstOrDefault(f => f.FullName.EndsWith("en_us.dic", StringComparison.InvariantCultureIgnoreCase)).FullName; //Path.Combine(dictionaryPath, "en_us.dic");
+                enConfig.HunspellKey = "";
+                SpellEngine.AddLanguage(enConfig);
+
+                enConfig = new LanguageConfig();
+                enConfig.LanguageCode = "bg";
+                enConfig.HunspellAffFile = files.FirstOrDefault(f => f.FullName.EndsWith("bg.aff", StringComparison.InvariantCultureIgnoreCase)).FullName; //Path.Combine(dictionaryPath, "en_us.aff");
+                enConfig.HunspellDictFile = files.FirstOrDefault(f => f.FullName.EndsWith("bg.dic", StringComparison.InvariantCultureIgnoreCase)).FullName; //Path.Combine(dictionaryPath, "en_us.dic");
+                enConfig.HunspellKey = "";
+                SpellEngine.AddLanguage(enConfig);
+
+
+            }
+            catch (Exception)
+            {
+                if (SpellEngine != null)
+                    SpellEngine.Dispose();
+
+                throw;
+            }
+        }
+    }
+}
